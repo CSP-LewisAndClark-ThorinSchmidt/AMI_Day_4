120672a3fdc639a4b78958a841a52fe985b12b62
Stephen 00GANTZ <90414323+StephenGantz@users.noreply.github.com>
2022-02-17 19:09:36 +0000

    Login.py and Main.py

--- login.py
+++ login.py
@@ -2,20 +2,32 @@
 # 
 # //22
 
-'''
-Login Class File 
+import tkinter as tk
+from tkinter import *
+from app import Application
 
-A simple login frame that is displayed first. If the correct password is entered (It has been set to Pa$$word for this example), the program will switch to the Application frame.
-
-BONUS: If the number of attempts exceeds 3, the program will exit. 
-'''
 
 class Login(tk.Frame):
-    """ A GUI login frame with a label, entry box, and login button """
+  """ A GUI login frame with a label, entry box, and login button """
+
+  login_tries = 0
 
   def __init__(self, master):
     """ Initialize the Frame. """
+    super(Login, self).__init__()
+    self.master = master
+    self.passwordEntry = Entry(self, width=50)
+    self.loginButton = Button(self, text="Enter Your Password", command = self.button_callback)
+    self.passwordEntry.pack()
+    self.loginButton.pack()
 
+  def button_callback(self):
+    self.login_tries += 1
 
+    if self.login_tries > 3:
+      exit()
 
-
+    if (self.passwordEntry.get() == "Pa$$word"):
+      self.destroy()
+      app = Application(self.master)
+      app.pack()
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 # main.py
-# Thorin Schmidt
+# Stephen Gantz and Gabriel Whitten
 # 02/01/22
 
 '''
@@ -9,27 +9,33 @@ Simple program to demonstrate how to create a login screen and switch frames in 
 '''
 
 import tkinter as tk
+from tkinter import *
 import sys
 from app import Application
-#from login import Login
+from login import Login
 
 class Root(tk.Tk):
   """Root class"""
-
   def __init__(self):
     super(Root, self).__init__()
     self.title("My Awesome App")
-    self.geometry("300x300")
+    self.geometry("800x450")
 
   def change_to_app(self):
     app_frame.pack(fill = 'both', expand = 1)
     login_frame.forget()
-  
-  
+
+
   def login_fail(self):
     sys.exit()
-  
+
 # main
+
+root = Root()
+frame = Login(root)
+frame.pack()
+root.mainloop()
+  
 # The frames are called login_frame, and app_frame
 # I'm pretty sure you can figure out which is which
 # Check the announcements channel for linksto helpful
