1e98074d636ade48c5938964f3ef513a2401d728
Jeffk98 <70586198+Jeffk98@users.noreply.github.com>
2022-02-17 11:15:29 -0600

    Add files via upload

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
+# !/usr/bin/env python3
+# Jeff Kimani
+# 2/17/22
 # main.py
-# Thorin Schmidt
-# 02/01/22
 
+#Partner Jacob Smith
 '''
 My Awesome App
 
@@ -11,7 +13,7 @@ Simple program to demonstrate how to create a login screen and switch frames in 
 import tkinter as tk
 import sys
 from app import Application
-#from login import Login
+from login import Login
 
 class Root(tk.Tk):
   """Root class"""
@@ -21,14 +23,29 @@ class Root(tk.Tk):
     self.title("My Awesome App")
     self.geometry("300x300")
 
+
+  def change_to_2(self):
+    login_frame.pack(fill = 'both', expand = 1)
+    app_frame.forget()
+
+
   def change_to_app(self):
     app_frame.pack(fill = 'both', expand = 1)
     login_frame.forget()
-  
+
   
   def login_fail(self):
     sys.exit()
-  
+
+# main
+root = Root()
+
+login_frame = Login(root)
+app_frame = Application(root) 
+#login_frame = Login(root)
+
+app_frame.pack(fill = 'both', expand = 1)
+
 # main
 # The frames are called login_frame, and app_frame
 # I'm pretty sure you can figure out which is which
