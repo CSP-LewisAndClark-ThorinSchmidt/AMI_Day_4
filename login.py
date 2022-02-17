dcca904348860b42e19a736cb66be3899c38d624
Austin Klemme <90398105+AustinKlem@users.noreply.github.com>
2022-02-17 10:04:38 -0600

    Add files via upload

--- login.py
+++ login.py
@@ -1,6 +1,6 @@
 # login.py
-# 
-# //22
+# Austin Klemme and Gavin Sutherland
+# 02/17/22
 
 '''
 Login Class File 
@@ -9,13 +9,32 @@ A simple login frame that is displayed first. If the correct password is entered
 
 BONUS: If the number of attempts exceeds 3, the program will exit. 
 '''
+import tkinter as tk
 
 class Login(tk.Frame):
-    """ A GUI login frame with a label, entry box, and login button """
+  """ A GUI login frame with a label, entry box, and login button """
 
   def __init__(self, master):
-    """ Initialize the Frame. """
+    """Initialize the Frame."""  
+    super(Login, self).__init__(master)
 
+    self.master = master
+    self.create_widgets()
 
+  def create_widgets(self):
+    '''Creates the widgets'''
+    self.lblOne = tk.Label(self, text = "Enter the Password")
+    self.lblOne.pack()
+    self.entry1 = tk.Entry(self)
+    self.entry1.pack()
+    self.btnOne = tk.Button(self, text = "Login", command =  self.check_pass)
+    self.btnOne.pack()
 
+  def check_pass(self):
+    '''Checks if the password is correct'''
+    pass_ent = self.entry1.get()
+    if pass_ent == "pa$$word":
+      self.master.change_to_app()
 
+    else:
+      self.master.login_fail()
