5166e180af9ae442820c38f7234f4aa36733a572
Josh2455 <jlyell@stcharlessd.org>
2022-02-22 10:59:04 -0600

    Add work

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 # app.py
-# Thorin Schmidt
-# 02/01/22
+# Josh Lyell, Andrew Vester
+# 02/17/22
 
 '''
 App Class File 
@@ -19,7 +19,6 @@ class Application(tk.Frame):
     """ Initialize the Frame. """
     
     super(Application, self).__init__(master)
-    self.grid()
     self.create_widgets()
 
   def create_widgets(self):
